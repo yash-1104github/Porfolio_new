9d101d68fdff805b7056ee607a614716a0d535fc
yash-1104github <gyah1104@gmail.com>
2025-09-05 19:13:07 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "@/pages/index"
 import NotFound from "@/pages/Notfound";
+import { Analytics } from "@vercel/analytics/next"
 
 const queryClient = new QueryClient();
 
@@ -21,6 +22,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+       <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
